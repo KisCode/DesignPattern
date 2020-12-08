167a3966396d3a57532069eaaca63d3c36f4257f
KENO <keno123@126.com>
2020-12-08 13:47:30 +0800

    Add old/java/javaee/ee dir

---
+++ 00_git/src/com/kiscode/old/OldStudent.java
@@ -0,0 +1,9 @@
+package com.kiscode.old;
+
+/**
+ * Description:
+ * Author: keno
+ * Date : 2020/12/8 13:40
+ **/
+public class OldStudent {
+}
---
+++ 00_git/src/com/kiscode/old/java/javaee/ee/Ee.java
@@ -0,0 +1,9 @@
+package com.kiscode.old.java.javaee.ee;
+
+/**
+ * Description:
+ * Author: keno
+ * Date : 2020/12/8 13:47
+ **/
+public class Ee {
+}
