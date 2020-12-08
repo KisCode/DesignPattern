d3142f31b6dd7d89da524e8cbb003c0ee2483ecc
KENO <keno123@126.com>
2020-12-08 13:31:33 +0800

    Add git mv test dir

---
+++ 00_git/src/com/kiscode/Test.java
@@ -0,0 +1,9 @@
+package com.kiscode;
+
+/**
+ * Description:
+ * Author: keno
+ * Date : 2020/12/8 13:30
+ **/
+public class Test {
+}
---
+++ 00_git/src/com/kiscode/old/Result.java
@@ -0,0 +1,9 @@
+package com.kiscode.old;
+
+/**
+ * Description:
+ * Author: keno
+ * Date : 2020/12/8 13:30
+ **/
+public class Result {
+}
